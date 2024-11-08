82abffb715c41738182d6fa06e198b3f627f35b2
Olivia <olivia.guyot@camptocamp.com>
2024-11-08 12:56:41 +0100

    read airflow API from ingestor

--- ingestor/app/main.py
+++ ingestor/app/main.py
@@ -1,8 +1,29 @@
-from typing import Union
-
+import http.client as http_client
+import logging
+import os
+from base64 import b64encode
 from fastapi import FastAPI
 
+from .airflow_client.airflow_api_stable_client import AuthenticatedClient
+from .airflow_client.airflow_api_stable_client.api.dag import get_dags
+from .airflow_client.airflow_api_stable_client.models import DAGCollection
+
+# this will log HTTP requests
+http_client.HTTPConnection.debuglevel = 1
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+airflow_url = os.environ.get("AIRFLOW_URL") or "http://localhost:8080"
+
 app = FastAPI()
+
+
+def get_client():
+    encoded_credentials = b64encode(f"airflow:airflow".encode()).decode()
+    client = AuthenticatedClient(base_url=airflow_url + "/api/v1", token=encoded_credentials).with_headers(
+        {"Content-Type": "application/json"})
+    client.prefix = "Basic"
+    return client
 
 
 @app.get("/")
@@ -10,6 +31,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/dags")
+def read_dags():
+    with get_client() as client:
+        collection: DAGCollection = get_dags.sync(client=client)
+        # map to an array of DAG ids
+        dag_ids = [dag.dag_id for dag in collection.dags]
+        return {"dags": dag_ids}
