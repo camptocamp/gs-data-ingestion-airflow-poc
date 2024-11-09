41283a6be608f720d479af9caf066a56438339c9
Olivia <olivia.guyot@camptocamp.com>
2024-11-10 00:35:55 +0100

    allow loading an external file into postgis
    try it out:
    http://127.0.0.1:8081/vrt?source=https://france-geojson.gregoiredavid.fr/repo/regions.geojson

--- ingestor/app/main.py
+++ ingestor/app/main.py
@@ -3,15 +3,18 @@ import logging
 import os
 from base64 import b64encode
 from fastapi import FastAPI
+from ogr2vrt_simple import HttpSource, FileSource
 
 from .airflow_client.airflow_api_stable_client import AuthenticatedClient
 from .airflow_client.airflow_api_stable_client.api.dag import get_dags
 from .airflow_client.airflow_api_stable_client.models import DAGCollection
 
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 # this will log HTTP requests
 http_client.HTTPConnection.debuglevel = 1
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
 
 airflow_url = os.environ.get("AIRFLOW_URL") or "http://localhost:8080"
 
@@ -38,3 +41,38 @@ def read_dags():
         # map to an array of DAG ids
         dag_ids = [dag.dag_id for dag in collection.dags]
         return {"dags": dag_ids}
+
+
+@app.get('/vrt')
+def generate_vrt(source: str):
+    config = {
+        "filename": "",
+        "relative_to_file": True,
+        "db_friendly": True,
+        "no_vsicurl": False,
+        "data_formats": "",
+        "template": "templates/vrt.j2",
+    }
+    data_source = source
+    vrt_factory = None
+    if source.startswith("http"):
+        vrt_factory = HttpSource(data_source, config)
+    else:
+        vrt_factory = FileSource(data_source, config)
+    vrt_xml = vrt_factory.build_vrt()
+    if vrt_xml:
+        with open("/tmp/current.vrt", "w") as f:
+            f.write(vrt_xml)
+            logger.info(f"VRT file written to /tmp/current.vrt")
+    else:
+        logger.error("error build VRT file")
+        return "error"
+
+    # convert VRT to PostGIS
+    os.system(
+        'ogr2ogr -f PostgreSQL PG:"dbname=postgis host=postgis port=5433 user=postgis password=postgis active_schema=public" /tmp/current.vrt')
+
+    logger.info("Data successfully loaded into PostGIS:")
+    logger.info(vrt_xml)
+
+    return {"status": "success"}
